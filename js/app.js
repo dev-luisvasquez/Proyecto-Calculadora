7b1a75d3464f4a877930c1c1d61ede7d05c58b66
th3fxcker <luchitoxpatrick13@gmail.com>
2021-09-08 22:30:39 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function init(){
+      var resultado = document.getElementById("resultado")
+      var uno = document.getElementById("uno")
+
+
+
+
+
+
+
+
+
+
+
+
+
+      uno.onclick = function(e){
+            resultado.textContent = resultado.textContent + "1";
+      }
+};
